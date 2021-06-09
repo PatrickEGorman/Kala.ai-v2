c16984290c1064f3960135e03d40cdb85d6c695b
Patrick Gorman <gormanpe@vcu.edu>
2021-06-09 14:17:17 -0400

    Added initial express apps to containers

---
+++ machines/src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import 'express-async-errors';
+import {json} from 'body-parser'
+import {NotFoundError, errorHandler} from "../../common/src";
+
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+
+app.all('*', async (req, res, next) => {
+    throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
+export {app};
