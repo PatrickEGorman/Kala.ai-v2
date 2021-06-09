9cc43dcb432e3204d8762686c1df42e6f5105cf0
Patrick Gorman <gormanpe@vcu.edu>
2021-06-09 14:25:05 -0400

    Setup for testing

--- factories/src/test/setup.ts
+++ factories/src/test/setup.ts
@@ -1,19 +1,9 @@
 import mongoose from "mongoose";
 import {MongoMemoryServer} from 'mongodb-memory-server';
-import jwt from 'jsonwebtoken';
-
-declare global {
-    namespace NodeJS {
-        interface Global {
-            signin(): [string]
-        }
-    }
-}
 
 let mongo: any;
 
 beforeAll(async () => {
-    process.env.JWT_KEY = 'fdgfg';
     mongo = new MongoMemoryServer();
     const mongoUri = await mongo.getUri();
 
@@ -34,20 +24,3 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 })
-
-global.signin = () => {
-    const payload = {
-        id: new mongoose.Types.ObjectId().toHexString(),
-        email: 'test@test.test'
-    }
-
-    const token = jwt.sign(payload, process.env.JWT_KEY!);
-
-    const session = {jwt: token};
-
-    const sessionJSON = JSON.stringify(session);
-
-    const base64 = Buffer.from(sessionJSON).toString('base64');
-
-    return [`express:sess=${base64}`]
-}
