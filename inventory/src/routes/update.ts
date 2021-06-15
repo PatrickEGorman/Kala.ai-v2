02285d9f1b6abb97d5be11eddec06698f0938211
Patrick Gorman <gormanpe@vcu.edu>
2021-06-15 00:23:04 -0400

    Changed update material method to increment or decriment quantity and avoid invalid input

--- inventory/src/routes/update.ts
+++ inventory/src/routes/update.ts
@@ -1,11 +1,18 @@
 import express, {Request, Response} from "express";
 import {NotFoundError, validateRequest} from "@kala.ai/common";
 import {Material} from "../models/Material";
+import {body} from "express-validator";
 
 
 const router = express.Router();
 
 router.post('/api/inventory/:id',
+    body("cost")
+        .default(0)
+        .isFloat({min:0}),
+    body('quantity')
+        .default(0)
+        .isFloat(),
     validateRequest,
     async (req: Request, res: Response) => {
         // todo: check operator authorization
@@ -14,20 +21,22 @@ router.post('/api/inventory/:id',
         if (!material) {
             throw new NotFoundError;
         }
-
-        let changed = false;
+        if (req.body.cost>0) {
+            material.set({cost: req.body.cost});
+        }
 
         if(req.body.quantity){
-            material.set({quantity:req.body.quantity})
-            changed = true;
+            const quantity = material.quantity + req.body.quantity
+            if(quantity>=0){
+                material.set({quantity:quantity});
+                await material.save();
+            }
+            else{
+                throw new Error(`Insufficient supplies.  Current quantity of ${material.name} in factory: 
+                    ${material.quantity}`)
+            }
         }
-        if(req.body.cost){
-            material.set({cost:req.body.cost})
-            changed = true
-        }
-        if(changed) {
-            await material.save()
-        }
+        material.save();
 
         res.status(200).send(material);
     });
