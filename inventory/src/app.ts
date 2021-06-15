4c7ac0a89c2a1bf7dff84e33733a505aa0e7b43e
Patrick Gorman <gormanpe@vcu.edu>
2021-06-14 23:15:54 -0400

    Added update material route and test

--- inventory/src/app.ts
+++ inventory/src/app.ts
@@ -4,6 +4,7 @@ import {json} from 'body-parser'
 import {NotFoundError, errorHandler} from "@kala.ai/common";
 import {createMaterialRouter} from "./routes/create";
 import {readMaterialRouter} from "./routes/read";
+import {updateMaterialRouter} from "./routes/update";
 
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(json());
 
 app.use(createMaterialRouter);
 app.use(readMaterialRouter);
+app.use(updateMaterialRouter);
 
 app.all('*', async (req, res, next) => {
     throw new NotFoundError();
---
+++ inventory/src/routes/update.ts
@@ -0,0 +1,34 @@
+import express, {Request, Response} from "express";
+import {NotFoundError, validateRequest} from "@kala.ai/common";
+import {Material} from "../models/Material";
+
+
+const router = express.Router();
+
+router.post('/api/inventory/:id',
+    validateRequest,
+    async (req: Request, res: Response) => {
+        // todo: check operator authorization
+        const material = await Material.findById(req.params.id);
+        if (!material) {
+            throw new NotFoundError;
+        }
+
+        let changed = false;
+
+        if(req.body.quantity){
+            material.set({quantity:req.body.quantity})
+            changed = true;
+        }
+        if(req.body.cost){
+            material.set({cost:req.body.cost})
+            changed = true
+        }
+        if(changed) {
+            await material.save()
+        }
+
+        res.status(200).send(material);
+    });
+
+export {router as updateMaterialRouter}
