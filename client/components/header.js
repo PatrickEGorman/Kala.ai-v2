48c1b14b70c5eb80e42507f288b7b230fc20bad6
Patrick Gorman <gormanpe@vcu.edu>
2021-06-15 14:53:46 -0400

    Initial work on building client-side inventory methods

---
+++ client/components/header.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default () => {
+
+    const links = [
+        {label: "Inventory", href: "/services/inventory/list"}
+    ].filter(linkConfig => linkConfig)
+        .map(({label, href}) => {
+            return <li key={href} className={"nav-item"}>
+                <Link href={href}>
+                    <a className={"nav-link"}>{label}</a>
+                </Link>
+            </li>;
+        });
+    return <nav className={"navbar navbar-ligth bg-light"}>
+        <Link href={"/"}>
+            <a className={"navbar-brand"}>GitTix</a>
+        </Link>
+
+        <div className={"d-flex justify-content-end"}>
+            <ul className="nav d-flex align-items-center">
+                {links}
+            </ul>
+        </div>
+    </nav>
+        ;
+};
