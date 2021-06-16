58e523bb4d5df72dfa65b4d2f898d249c8b84e13
Patrick Gorman <gormanpe@vcu.edu>
2021-06-15 21:09:52 -0400

    Removed current user instances from code

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,15 +4,12 @@ import Header from "../components/header";
 
 const AppComponent = ({Component, pageProps, currentUser}) => {
     return <div>
-        <Header currentUser={currentUser}/>
+        <Header/>
         <Component {...pageProps}/>;
     </div>;
 };
 
 AppComponent.getInitialProps = async appContext => {
-    const client = buildClient(appContext.ctx);
-    const {data} = await client.get("/api/users/currentuser");
-
     let pageProps = {};
     if (appContext.Component.getInitialProps) {
         pageProps = await appContext.Component.getInitialProps(appContext.ctx);
