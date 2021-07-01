1d43dd1596bfcef3d5f37f73883ae09cf7070c5e
Patrick Gorman <gormanpe@vcu.edu>
2021-07-01 13:16:51 -0400

    Created materials drop down menu

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,22 @@
 import "bootstrap/dist/css/bootstrap.css";
+import Head from "next/head";
 import buildClient from "../api/build-client";
 import Header from "../components/header";
+import {Container} from "react-bootstrap";
 
 const AppComponent = ({Component, pageProps}) => {
-    return <div>
-        <Header/>
-        <div className={"container"}>
-            <Component {...pageProps}/>
-        </div>
-    </div>;
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            </Head>
+            <div>
+                <Header/>
+                <Container>
+                    <Component {...pageProps}/>
+                </Container>
+            </div>
+        </>);
 };
 
 AppComponent.getInitialProps = async appContext => {
