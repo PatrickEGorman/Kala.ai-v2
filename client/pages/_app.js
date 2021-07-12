e5f78c2d55afe982ac84aae87e5ea537522a6855
Patrick Gorman <gormanpe@vcu.edu>
2021-07-12 12:23:01 -0400

    Minor tweaks for deployment

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import Head from "next/head";
 import buildClient from "../api/build-client";
 import Header from "../components/header";
-import {Col, Container, Row} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 
 const AppComponent = ({Component, pageProps}) => {
     const title = pageProps.title || undefined;
@@ -12,16 +12,10 @@ const AppComponent = ({Component, pageProps}) => {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <title>{title || "Kala.ai V2"}</title>
             </Head>
-            <Row className={"h-100"}>
-                <Col md={2}>
-                    <Header/>
-                </Col>
-                <Col md={9}>
-                    <div className={"mr-3"}>
-                        <Component {...pageProps}/>
-                    </div>
-                </Col>
-            </Row>
+            <Header/>
+            <Container>
+                <Component {...pageProps}/>
+            </Container>
         </>);
 };
 
