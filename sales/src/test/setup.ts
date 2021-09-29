cd53654c7d4ba2a3b79e6b480fcb90ef9cfce31f
Patrick Gorman <gormanpe@vcu.edu>
2021-09-28 20:45:42 -0400

    Fixed depreciation issue

--- sales/src/test/setup.ts
+++ sales/src/test/setup.ts
@@ -17,7 +17,9 @@ beforeAll(async () => {
 
     await mongoose.connect(mongoUri, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
     });
 });
 
